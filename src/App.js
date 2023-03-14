d5cb35b92a1568d2f081c2b16d51115efcf50179
Ben663 <87435130+Ben663@users.noreply.github.com>
2023-03-14 18:47:41 +0200

    start app with openAi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+// import dotnev from 'dotenv'
+import { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
+// dotnev.config();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const configuration = new Configuration({
+		apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+	});
+	const openai = new OpenAIApi(configuration);
 
+	const [prompt, setPrompt] = useState('');
+	const [result, setResult] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	const handleClick = async () => {
+		setLoading(true);
+		try {
+			const response = await openai.createCompletion({
+				model: 'text-davinci-003',
+				prompt: prompt,
+				temperature: 0.5,
+				max_tokens: 100,
+			});
+			setResult(response.data.choices[0].text);
+		} catch (error) {
+			console.error(error);
+		}
+		setLoading(false);
+	};
+	return (
+		<main className='main'>
+			<div className='w-2/4 mx-auto'>
+				<textarea
+					type='text'
+					value={prompt}
+					onChange={(e) => setPrompt(e.target.value)}
+					placeholder='write your prompt..'
+					className='textarea'></textarea>
+				<button
+					onClick={handleClick}
+					disabled={loading || prompt.length === 0}
+					className='btn'>
+					{loading ? 'Generating... ' : 'Generate'}
+				</button>
+				<pre className='result'>{result}</pre>
+			</div>
+		</main>
+	);
+}
 export default App;
